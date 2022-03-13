2b1662de904b9892069b6dde499a833761a9850b
3ddelano <3ddelano@gmail.com>
2022-03-13 20:19:11 +0530

    fix plugin.gd

--- addons/modular-tester-godot/plugin.gd
+++ addons/modular-tester-godot/plugin.gd
@@ -10,15 +10,15 @@ Copyright (c) 2022 Delano Lourenco
 """
 
 func _enter_tree():
-	# Ensure the addon in placed in res://addons/tester
+	# Ensure the addon in placed in res://addons/modular-tester-godot
 	var path: String = get_script().get_path()
 	var base_dir = path.get_base_dir()
 
-	if base_dir != "res://addons/tester":
-		var found_folder = base_dir.substr(0, base_dir.length() - "tester".length())
-		printerr("Error: tester folder must be in res://addons/ but it was found in %s" % found_folder)
+	if base_dir != "res://addons/modular-tester-godot":
+		var found_folder = base_dir.substr(0, base_dir.length() - "modular-tester-godot".length())
+		printerr("Error: modular-tester-godot folder must be in res://addons/ but it was found in %s" % found_folder)
 
-	add_autoload_singleton("TestUtils", "res://addons/tester/test_utils.gd")
+	add_autoload_singleton("TestUtils", "res://addons/modular-tester-godot/test_utils.gd")
 
 func _exit_tree() -> void:
 	remove_autoload_singleton("TestUtils")
